b2065472a15352ee05790852e4387b0714e29085
Prelang Builder <builder@prelang.com>
2015-10-28 16:45:03 +0000

    Generated model Activity with migration

---
+++ db/migrate/20151028164503_create_activities.rb
@@ -0,0 +1,11 @@
+class CreateActivities < ActiveRecord::Migration
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.string :implementer
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
